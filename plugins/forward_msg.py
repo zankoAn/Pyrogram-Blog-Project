4624ccdae86fada97435df47890603f2907c3d97
zankoatzanko <zankoan4@gmail.com>
2021-08-28 19:50:31 +0430

    Update forward_msg

--- plugins/forward_msg.py
+++ plugins/forward_msg.py
@@ -1,70 +1,110 @@
 from pyrogram import Client, filters
+
 from plugins import r, admin
 
-import time
+import asyncio
+
+
 
 
 @Client.on_message(filters.private & filters.user(admin) & filters.command("forward"))
-def forward_msg_S1(c, m):
-    c.send_chat_action(m.chat.id, 'typing')
-    time.sleep(0.15)
-    c.send_message(m.chat.id, """
-    ☔️ برای ارسال پیام به تعداد مشخصی از یوزر ها  یک `عدد` را ارسال کنید.
-☔️ برای ارسال فوروارد به همه یوزر ها  علامت  `*` را ارسال کنید.
-    .
-    """)
+async def forward_msg_S1(c, m):
+
+    message = """⚡️ **Forward Msg** ⚡️
+        \n☔️ برای ارسال پیام به تعداد مشخصی از یوزر ها  یک `عدد` را ارسال کنید
+        \n☔️ برای ارسال فوروارد به همه یوزر ها  علامت  `*` را ارسال کنید
+        \n.    
+    """
     
-    r.set("forward_msg", "step1")
-
-
-@Client.on_message(filters.private & filters.user(admin), group=2)
-def forward_msg_S2(c, m):
     chat_id = m.chat.id 
-    message = m.text
 
-    if r.get("forward_msg") == "step1" and message != "/forward":
+    await c.send_chat_action(chat_id=chat_id, action="typing")
+    await asyncio.sleep(0.15)
+  
+    await c.send_message(chat_id=chat_id, text=message)
+    
+    # First Step
+    r.setbit("forward:msg", 0, 1)
+
+    # Second Step
+    r.setbit("forward:msg", 1, 0)
+
+
+
+
+@Client.on_message(filters.private & filters.user(admin) , group=4)
+async def forward_msg_S2(c, m):
+
+    chat_id = m.chat.id
+
+    msg = m.text
+
+    if r.getbit("forward:msg", 0) == 1 and msg and not msg.startswith("/"):
         try:
-            if message == "*" or isinstance(int(message), int):
-                c.send_chat_action(chat_id=chat_id, action='typing')
-                time.sleep(0.15)
+            if msg == "*" or int(msg):
+                await c.send_chat_action(chat_id=chat_id, action="typing")
+                await asyncio.sleep(0.15)
 
-                resp = c.send_message(chat_id=chat_id, text="پیام مورد نظر خود را برای فوروارد کردن ارسال نمایید.")
+                resp = await c.send_message(chat_id=chat_id, 
+                    text="""⚡️ **Forward Msg** ⚡️
+                        \nپیام مورد نظر خود را برای فوروارد کردن ارسال نمایید
+                        \n.
+                    """
+                )
                 
-                r.set("next_msg", resp.message_id)
-                r.set("count", message)
-                r.set("forward_msg", "step2")
+                r.set("Msg:Number", msg)
+                r.set("Previous:Msg:ID", resp.message_id)
+                r.setbit("forward:msg", 0, 0)
+                r.setbit("forward:msg", 1, 1)
 
         except ValueError:
-            c.send_chat_action(chat_id=chat_id, action='typing')
-            time.sleep(0.10)
-            c.send_message(chat_id=chat_id, text="❌ ورودی شما اشتباه است\nلطفا فرمت مشخص شده را رعایت کنید.\n.")
+            await c.send_chat_action(chat_id=chat_id, action='typing')
+            await asyncio.sleep(0.10)
 
-
-@Client.on_message(filters.private & filters.user(admin), group=3)
-def forward_msg_S3(c, m):
-    chat_id = m.chat.id 
-    
-    if r.get("forward_msg") == "step2" and int(m.message_id) == int(r.get("next_msg")) + 1:
-        counter = 1
-
-        for user_id in r.smembers("users"):
-            c.send_chat_action(chat_id=chat_id, action='typing')
-            time.sleep(0.10)
-
-            c.forward_messages(
-                chat_id = user_id,
-                from_chat_id = chat_id,
-                message_ids = m.message_id
+            await c.send_message(chat_id=chat_id, 
+                text="""⚡️ **Forward Msg** ⚡️
+                    \nورودی صحیح نمیباشد! ❌
+                    \nلطفا فرمت مشخص شده را رعایت کنید.                    
+                    \n.
+                """
             )
 
-            if r.get("count") != "*" and counter == int(r.get("count")):
-                break
 
-            else:
-                counter += 1
 
-        c.send_chat_action(chat_id=chat_id, action='typing')
-        time.sleep(0.10)
-        c.send_message(chat_id=chat_id, text="پیام شما با موفقت ارسال شد ✅")
 
-        r.set("forward_msg", "step3")
+
+@Client.on_message(filters.private & filters.user(admin)&~ filters.me, group=5)
+async def forward_msg_S3(c, m):
+
+    chat_id = m.chat.id 
+    
+    if r.getbit("forward:msg", 1) == 1:
+
+        previous_msg = int(r.get("Previous:Msg:ID"))
+
+        if m.message_id == int(previous_msg)+1:                
+            counter = 1
+            current_user = await c.get_me()
+
+            for user_id in r.smembers(f"users:{current_user.id}"):
+                await c.send_chat_action(chat_id=chat_id, action='typing')
+                await asyncio.sleep(0.10)
+
+                await c.forward_messages(
+                    chat_id = user_id,
+                    from_chat_id = chat_id,
+                    message_ids = m.message_id
+                )
+
+                if r.get("Msg:Number") != "*" and counter == int(r.get("Msg:Number")):
+                    break
+
+                else:
+                    counter += 1
+
+            await c.send_chat_action(chat_id=chat_id, action='typing')
+            await asyncio.sleep(0.10)
+            await c.send_message(chat_id=chat_id, text="پیام شما با موفقت ارسال شد ✅")
+
+            r.setbit("forward:msg", 1, 0)
+
